29782bb1211d71d7b4557248637c2603a1ba9931
John Young <theroad2eldorado@gmail.com>
2022-03-01 22:38:43 -0500

    updates

--- pages/api/courses.js
+++ pages/api/courses.js
@@ -1,5 +1,7 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
 export default function handler(req, res) {
-  res.status(200).json(JSON.stringify(data))({
+  res.status(200).json({
     courses: [
       {
         name: 'Oak Shadows',
